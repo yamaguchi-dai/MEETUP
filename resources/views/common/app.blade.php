d9a03742933e4d5f11ca255ed8e18709f935e084
yamaguppy31 <yamaguchi.dai31@gmail.com>
2018-01-21 19:50:20 +0900

    ヘッダー修正

--- resources/views/common/app.blade.php
+++ resources/views/common/app.blade.php
@@ -4,12 +4,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=yes">
         <!-- CSS  -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-        <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-      
+        <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+        <link href="/css/common/header.css" type="text/css" rel="stylesheet" >
+
     </head>
     <body>
-        <nav class="light-blue lighten-1" role="navigation">
+        <nav class="red lighten-1" role="navigation">
             <div class="nav-wrapper">
                 <a href="/" class="brand-logo center">MEETUP</a>
                 <ul id="nav-mobile" class="left">
@@ -17,7 +17,7 @@
                 </ul>
             </div>
         </nav>
-       
+
         <!-- メインコンテンツエリア -->
         <div class="container">
             @yield('content')
---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,10 @@
+@extends('common.app')
+@section('title', '')
+@push('scripts')
+<script></script>
+@endpush
+@section('content')
+
+
+
+@endsection
